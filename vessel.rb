15cc3c1e3ef08eb7581efb95d384f1c566aa3faf
Jake Franklin <jacob.d.franklin@gmail.com>
2024-06-10 11:44:19 -0700

    Linting fixes on vessel.rb

--- vessel.rb
+++ vessel.rb
@@ -1,7 +1,8 @@
 #!/bin/env ruby
-# encoding: utf-8
+
 require 'benchmark'
 
+# The Paradise Vessel, mostly used to serve HTML
 class VesselParadise
 
   include Vessel
@@ -10,21 +11,21 @@ class VesselParadise
 
     super
 
-    @path = File.expand_path(File.join(File.dirname(__FILE__), "/"))
+    @path = File.expand_path(File.join(File.dirname(__FILE__), '/'))
 
     load_folder("#{@path}/objects/*")
     load_folder("#{@path}/vessels/*")
 
-    @name = "Paradise"
-    @docs = "A multiplayer interactive fiction multiverse"
+    @name = 'Paradise'
+    @docs = 'A multiplayer interactive fiction multiverse'
 
-    @@forum    = Memory_Array.new("forum", @path)
-    @@paradise = Memory_Array.new("paradise", @path)
-    @@parade   = @@paradise.to_a("teapot")
+    @@forum    = Memory_Array.new('forum', @path)
+    @@paradise = Memory_Array.new('paradise', @path)
+    @@parade   = @@paradise.to_a('teapot')
     @@player_id = nil
     @tunnels = nil
 
-    $nataniev.require("corpse","http")
+    $nataniev.require('corpse', 'http')
     install(:generic, :serve, CorpseHttp.new(self))
     install(:generic, :help)
 
@@ -36,52 +37,51 @@ class VesselParadise
 
     def @corpse.build
 
-      add_meta("description","Multiplayer Interactive Fiction Multiverse")
-      add_meta("keywords","paradise maeve")
-      add_meta("viewport","width=device-width, initial-scale=1, maximum-scale=1")
-      add_meta("apple-mobile-web-app-capable","yes")
-      add_meta("apple-touch-fullscreen","yes")
-      add_meta("apple-mobile-web-app-status-bar-style","black-translucent")
+      add_meta('description', 'Multiplayer Interactive Fiction Multiverse')
+      add_meta('keywords', 'paradise maeve')
+      add_meta('viewport', 'width=device-width, initial-scale=1, maximum-scale=1')
+      add_meta('apple-mobile-web-app-capable', 'yes')
+      add_meta('apple-touch-fullscreen', 'yes')
+      add_meta('apple-mobile-web-app-status-bar-style', 'black-translucent')
 
-      add_link("reset.css", :lobby)
-      add_link("font.input_mono.css", :lobby)
-      add_link("font.lora.css", :lobby)
-      add_link("style.fonts.css")
-      add_link("style.main.css")
+      add_link('reset.css', :lobby)
+      add_link('font.input_mono.css', :lobby)
+      add_link('font.lora.css', :lobby)
+      add_link('style.fonts.css')
+      add_link('style.main.css')
 
-      add_script("core/htmx.js", :lobby)
-      add_footer_script("main.js")
+      add_script('core/htmx.js', :lobby)
+      add_footer_script('main.js')
 
     end
 
-    def @corpse.query q = nil
+    def @corpse.query(q = nil)
 
+      @@parade = @@paradise.to_a('teapot')
 
-      @@parade = @@paradise.to_a("teapot")
-
-      parts = q.gsub("+", " ").strip.split(" ")
+      parts = q.gsub('+', ' ').strip.split(' ')
       vessel_id = parts.first.to_i
       @@player_id = vessel_id < 1 ? select_random_vessel : vessel_id
 
-      @action = parts[1] ? parts[1] : "look"
+      @action = parts[1] || 'look'
 
       # It's not pretty, but we need to alias the base help action on Paradise vessels
-      if @action == 'help' then @action = 'actions' end
+      @action = 'actions' if @action == 'help'
 
-      @params = parts.join(" ").sub(@@player_id.to_s, "").sub(@action, "").strip
+      @params = parts.join(' ').sub(@@player_id.to_s, '').sub(@action, '').strip
       @player = @@parade[@@player_id]
       @title  = "Paradise ∴ #{@player}"
       silent = @player.parent.is_silent
       silence = silent ? 'class="silent" ' : ''
 
-      @body = %Q(
+      @body = %(
       <bg></bg>
       <ui id="ui">
-        <inventory #{silence}id="inventory">#{self.create_inventory(@player.children)}</inventory>
+        <inventory #{silence}id="inventory">#{create_inventory(@player.children)}</inventory>
         <view #{silence}>
           #{@player.act(@action, @params)}
         </view>
-        <chat #{silence}id="chat">#{self.chat}</chat>
+        <chat #{silence}id="chat">#{chat}</chat>
 
         <form hx-post='/'
               hx-target='view'
@@ -123,72 +123,104 @@ class VesselParadise
 
       candidates = []
       @@parade.each do |vessel|
-        if vessel.rating > 0 then next end
+
+        next if vessel.rating > 0
+
         candidates.push(vessel)
+
       end
-      return "<meta http-equiv='refresh' content='0; url=/#{candidates.length > 0 ? candidates[rand(candidates.length)].memory_index : rand($parade.length)}'/>"
+      "<meta http-equiv='refresh' content='0; url=/#{candidates.length > 0 ? candidates[rand(candidates.length)].memory_index : rand($parade.length)}'/>"
 
     end
 
-    def @corpse.paradise; return @@paradise; end
-    def @corpse.parade; return @@parade; end
-    def @corpse.parade=(new_parade); @@parade = new_parade; return @@parade; end
-    def @corpse.player; return @player; end
-    def @corpse.forum; return @@forum; end
-    def @corpse.create_list arr, id = nil
+    def @corpse.paradise
+      @@paradise
+    end
+
+    def @corpse.parade
+      @@parade
+    end
+
+    def @corpse.parade=(new_parade)
+      @@parade = new_parade
+      @@parade
+    end
+
+    def @corpse.player
+      @player
+    end
+
+    def @corpse.forum
+      @@forum
+    end
+
+    def @corpse.create_list(arr, id = nil)
 
       html = "<ul #{id.nil? == false ? 'id="' + id + '" ' : ''}class='basic'>"
       arr.each do |item|
-        html += "<li>#{item.to_s}</li>"
+
+        html += "<li>#{item}</li>"
+
       end
-      html += "</ul>"
+      html += '</ul>'
 
-      return html
+      html
     end
-    def @corpse.create_inventory vessels
 
-      arr = vessels.map {|vessel| vessel.to_html("drop #{vessel.name}")}
-      html = "<h4>Carrying</h4>"
+    def @corpse.create_inventory(vessels)
+
+      arr = vessels.map { |vessel| vessel.to_html("drop #{vessel.name}") }
+      html = '<h4>Carrying</h4>'
       html += create_list arr, 'carrying'
-      return html
+      html
 
     end
+
     def @corpse.chat
 
-        html = "<h4>Forum</h4>"
+      html = '<h4>Forum</h4>'
 
-        if self.player.parent.is_silent then return "" end
+      return '' if player.parent.is_silent
 
-        messages = @@forum.to_a("comment")
+      messages = @@forum.to_a('comment')
 
-        selection = self.player.parent.name.like("lobby") ? messages[messages.length - 7, 7] : messages[messages.length - 3, 3]
+      selection = if player.parent.name.like('lobby')
+                    messages[messages.length - 7,
+                             7]
+                  else
+                    messages[messages.length - 3, 3]
+                  end
 
-        arr = selection.map{|message| message.to_s}
+      arr = selection.map { |message| message.to_s }
 
-        html += create_list arr, 'forum'
+      html += create_list arr, 'forum'
 
-        return "<ul id='forum'>#{html}</ul>"
+      "<ul id='forum'>#{html}</ul>"
 
     end
   end
 
   def tunnels
 
-    if @tunnels then return @tunnels end
+    return @tunnels if @tunnels
 
     @tunnels = []
     @@parade.each do |vessel|
-      if !vessel.is_tunnel then next end
+
+      next unless vessel.is_tunnel
+
       @tunnels.push(vessel)
+
     end
 
-    return @tunnels
+    @tunnels
 
   end
 
   def get_player_id
 
-    return @@player_id
+    @@player_id
 
   end
+
 end
